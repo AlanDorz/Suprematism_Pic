8a36736f6414201105d8029e0a640c273c26c2bf
AlanDorz <JonyTest007@gmail.com>
2020-02-27 23:34:29 +0300

    add animation by click

--- javascript/main.js
+++ javascript/main.js
@@ -33,3 +33,85 @@ let boxBtn01 =
   document.querySelector('.box')
 
 boxBtn01.onclick = boxScale.play
+
+// --TRIANGLE ANIMATION--
+
+let triangleScale = anime.timeline({
+  targets: '.box',
+  scale: 2.5,
+  rotate: 191.2,
+  translateX: -80,
+  translateY: -90,
+  duration: 2000,
+  endDelay: 900,
+  direction: 'alternate',
+  autoplay: false
+})
+
+triangleScale.add({
+  targets: '.triangle',
+  scale: 2.5,
+  rotate: 175,
+  translateY: 35,
+  duration: 1800
+}, 0)
+
+triangleScale.add({
+  targets: '.circle',
+  scale:0.7,
+  rotate: 175,
+  translateY: 315,
+  translateX: 460
+}, 0)
+
+let triangleBtn01 =
+  document.querySelector('.triangle')
+
+triangleBtn01.onclick = triangleScale.play
+
+// --CIRCLE ANIMATION--
+
+let circleScale = anime.timeline({
+  targets: '.box',
+  scale: 0.7,
+  rotate: 191.2,
+  translateX: -625,
+  translateY: -280,
+  duration: 2000,
+  endDelay: 900,
+  direction: 'alternate',
+  autoplay: false
+})
+
+circleScale.add({
+  targets: '.triangle',
+  scale: 0.7,
+  rotate: 175,
+  translateY: 35,
+  translateX: 300,
+  duration: 1800
+}, 0)
+
+circleScale.add({
+  targets: '.circle',
+  scale: 2.5,
+  rotate: 175,
+  translateY: 80,
+  translateX: 20
+}, 0)
+
+let circleBtn01 =
+  document.querySelector('.circle')
+
+circleBtn01.onclick = circleScale.play
+
+// ----MAGICSCROLL----
+
+
+
+
+
+
+
+
+//
